724a96e75f26ff65603509ac0593a53b58dbca1f
Thavasumoorthi <harshini.b@tringapps.com>
2024-12-30 16:56:36 +0530

    I removed only in the app.js file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app=express()
 
 
 app.get("/",(req,res)=>{
-    res.json("The app is developed using expressjs framework only")
+    res.json("The app is developed using expressjs framework ")
 })
 
 
